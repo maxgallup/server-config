1de51b372d6ba289a5454bbb2e7e8454a106304c
maxgallup <m.gallup@student.vu.nl>
2023-04-13 18:04:47 +0200

    another fix

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ mv $1/.bash_profile $1/.bash_profile_bak || echo "$1/.bash_profile didn't exist"
 mv $1/.profile $1/.profile_bak || echo "$1/.profile didn't exist"
 
 echo "Linking new files to $1"
-ln -s .bashrc $1/.bashrc
-ln -s .bash_logout $1/.bash_logout
-ln -s .bash_aliases $1/.bash_aliases
-ln -s .profile $1/.profile
+ln -s $1/.config/server-config/.bashrc $1/.bashrc
+ln -s $1/.config/server-config/.bash_logout $1/.bash_logout
+ln -s $1/.config/server-config/.bash_aliases $1/.bash_aliases
+ln -s $1/.config/server-config/.profile $1/.profile
 
