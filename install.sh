3e357cc8b2299f61ddb787413e719b4fb1f2b702
maxgallup <m.gallup@student.vu.nl>
2023-04-13 17:51:55 +0200

    will catch when mv fails

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 echo "Using $1 as home directory"
 
 echo "Backing up old files"
-mv $1/.bashrc $1/.bashrc_bak 
-mv $1/.bash_logout $1/.bash_logout_bak 
-mv $1/.bash_aliases $1/.bash_aliases_bak 
-mv $1/.bash_profile $1/.bash_profile_bak 
-mv $1/.profile $1/.profile_bak 
+mv $1/.bashrc $1/.bashrc_bak || echo "$1/.bashrc didn't exist"
+mv $1/.bash_logout $1/.bash_logout_bak || echo "$1/.bash_logout didn't exist"
+mv $1/.bash_aliases $1/.bash_aliases_bak || echo "$1/.bash_aliases didn't exist"
+mv $1/.bash_profile $1/.bash_profile_bak || echo "$1/.bash_profile didn't exist"
+mv $1/.profile $1/.profile_bak || echo "$1/.profile didn't exist"
 
-echo "Linking new files to /home/$1"
-ln -s .bashrc /home/$1/.bashrc
-ln -s .bash_logout /home/$1/.bash_logout
-ln -s .bash_aliases /home/$1/.bash_aliases
-ln -s .profile /home/$1/.profile
+echo "Linking new files to $1"
+ln -s .bashrc $1/.bashrc
+ln -s .bash_logout $1/.bash_logout
+ln -s .bash_aliases $1/.bash_aliases
+ln -s .profile $1/.profile
 
